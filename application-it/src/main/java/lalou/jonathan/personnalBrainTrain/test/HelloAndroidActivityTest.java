4cc6202ed192c0fcc425b5f06a2326aefcb6578e
JonathanLalou <jonathan.lalou@gmail.com>
2013-04-11 16:15:51 +0200

    initial commit

---
+++ application-it/src/main/java/lalou/jonathan/personnalBrainTrain/test/HelloAndroidActivityTest.java
@@ -0,0 +1,17 @@
+package lalou.jonathan.personnalBrainTrain.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import lalou.jonathan.personnalBrainTrain.HelloAndroidActivity;
+
+public class HelloAndroidActivityTest extends ActivityInstrumentationTestCase2<HelloAndroidActivity> {
+
+    public HelloAndroidActivityTest() {
+        super(HelloAndroidActivity.class);
+    }
+
+    public void testActivity() {
+        HelloAndroidActivity activity = getActivity();
+        assertNotNull(activity);
+    }
+}
+
